fd23f8160abb6fc87636deeda0306e1880f687ee
Dylan Ngo <it.tinhngo@gmail.com>
2022-11-18 09:14:00 +0800

    update code

--- public/js/qrcode.js
+++ public/js/qrcode.js
@@ -1,13 +1,14 @@
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
+    $("#qrcode").val(decodedText);
 }
 
 function onScanError(errorMessage) {
     // handle on error condition, with error message
     console.log(`Scan result: ${errorMessage}`);
+    $("#qrcode").val("");
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader", { fps: 10, qrbox: 250 });
+var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
