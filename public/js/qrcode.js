3cba6ec6155ce307a817e6c47a5f033969cbfffd
Dylan Ngo <it.tinhngo@gmail.com>
2022-11-18 12:02:54 +0800

    init

--- public/js/qrcode.js
+++ public/js/qrcode.js
@@ -1,20 +1,37 @@
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     console.log(`Scan result: ${decodedText}`, decodedResult);
-    $("#qr-code").val(decodedText);
+    $('#qr-code').val(decodedText);
+    $('#exampleModal').modal('show');
+    html5QrcodeScanner.clear();
 }
 
 function onScanFailure(errorMessage) {
     // handle on error condition, with error message
     console.log(`Scan result: ${errorMessage}`);
-    $("#qr-code").val(errorMessage);
+    $('#qr-code').val(errorMessage);
+    // $('#exampleModal').modal('show');
 }
 
-// Note: If you change the DOM element id here (example: "html5_qrcode_reader")
-// Also, change this in ../html5-qrcode-wp.php.
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var qrcode = $('#qr-code').val();
+
+    var modal = $(this);
+    modal.find('.modal-title').text('Confirm Form');
+    modal.find('.modal-body .text').text('Order Number: ' + qrcode);
+});
+
+$('#push-order-to-detrack').click(() => {
+    console.log('hi');
+    $('#exampleModal').modal('toggle');
+});
+
 let html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader",
-    // TODO(mebjas): Load all these values using data arguments in DOM element.
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
+    'qr-reader',
+    {
+        fps: 10,
+        qrbox: {width: 250, height: 250}
+    },
+    /* verbose= */ false
+);
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
